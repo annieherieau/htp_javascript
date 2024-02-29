b60c543e2d4d50bc48f6035c5925718536f3d8cb
annieherieau <annie.herieau@gmail.com>
2024-02-29 17:26:55 +0100

    script 4

--- scripts/script_4.js
+++ scripts/script_4.js
@@ -1,10 +1,8 @@
-console.log("****** Script 4 : Startup Nation ******");
+
 
 // nés dans les années 70: filtrer [1970-1980[
 function seventies(array) {
   let result = array.filter((val) => val.year >= 1970 && val.year < 1980);
-  console.log("Entrepreneurs nés dans les années 70:");
-  console.log(result);
   return result;
 }
 
@@ -69,47 +67,43 @@ function getUserInput() {
 
 // affichage de l'array
 function print(array) {
-  let string = "";
+  let string = "<ul>";
   for (let value of array) {
     if (typeof value == "string") {
-      string += value + "\n";
+      string += "<li>" + value + "</li>";
     } else {
-      string += `${value.first} ${value.last} (${value.year})`;
+      string += "<li>" + `${value.first} ${value.last} (${value.year})`;
       if (value.age) {
         string += ` Age: ${value.age} ans`;
       }
-      string += "\n";
+      string += "</li>";
     }
   }
+  string += "</ul>";
   return string;
 }
 
 // perform menu et choix user pour chaque fonction
 function perform(){
+  TITLE.innerHTML = "Script 4 : Startup Nation";
+  
   let user_input = getUserInput();
     switch (user_input) {
       case '1': 
-        alert("Nés dans les années 70:\n" + print(seventies(entrepreneurs)));
-        perform();
+        OUTPUT_1.innerHTML= "<strong>Entrepreneurs nés dans les années 70:</strong><br><br>" + print(seventies(entrepreneurs));
         break;
       case '2': 
-        alert("Prénoms et Noms : \n" + print(names(entrepreneurs)));
-        perform();
+        OUTPUT_1.innerHTML= "<strong>Prénoms et Noms :</strong><br><br>" + print(names(entrepreneurs));
+
         break;
       case '3': 
-        alert("Ages : \n" + print(ages(entrepreneurs)));
-        perform();
+        OUTPUT_1.innerHTML= "<strong>Ages :</strong><br><br>" + print(ages(entrepreneurs));
         break;
       case '4': 
-        alert("Ordre alphabetique des noms : \n" + print(alphaOrder(entrepreneurs)));
-        perform();
+        OUTPUT_1.innerHTML= "<strong>Ordre alphabetique des noms :</strong><br><br>" + print(alphaOrder(entrepreneurs));
         break;
-      case 'q':
-        break; 
       default:
-        perform()
         break;
-    
   }
 
 }
