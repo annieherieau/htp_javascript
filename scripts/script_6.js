dbe38dd1620096d16e64898207ec31f4226e29ee
annieherieau <annie.herieau@gmail.com>
2024-02-29 11:17:19 +0100

    script 6

--- scripts/script_6.js
+++ scripts/script_6.js
@@ -1,1 +1,40 @@
-console.log("****** Script 6 ******");
+
+// décoper ARN par triplet (return array)
+function getCodons(rna_string){
+  return rna_string.match(/.{1,3}/g) || []; // séparation tous les 3 caractères 
+}
+
+// recherche du codon dans l'array rna
+function decodeRna(codons_array, acids_array=amino_acids_data){
+
+  let result = codons_array.map(
+    (codon) => {
+      // console.log('codon :' + codon);
+      for (const acid of acids_array) {
+        if ((acid.codons.includes(codon))){
+          // console.log('A-Acid : ' + acid.name);
+          return acid.name;
+        }
+      }
+    }
+  )
+  // console.log(print(result));
+  return result;
+}
+
+// renvoyer string des Acides aminés
+function print(array){
+  return array.join('-');
+}
+
+function perform(rna){
+  string = `****** Script 6 : Code la vie ******\n${rna}\n`;
+  let codons = getCodons(rna);
+  let amino_acids = decodeRna(codons);
+  string += `${print(codons)}\n${print(amino_acids)}`;
+  console.log(string)
+  return string;
+}
+
+alert(perform('CCGUCGUUGCGCUACAGC'));
+alert(perform('CCUCGCCGGUACUUCUCG'));
